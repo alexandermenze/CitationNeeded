18d7bd69c10e9f51feb5cb21de1a65a1adb55b39
Alexander Menze <a.menze@polipol.de>
2019-09-09 12:26:25 +0200

    Created layout and viewstart pages

--- CitationNeeded.WebApp/Pages/Index.cshtml.cs
+++ CitationNeeded.WebApp/Pages/Index.cshtml.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
 
 namespace CitationNeeded.WebApp.Pages
 {
     public class IndexModel : PageModel
     {
+        public IEnumerable<string> BlaBla { get; set; }
+
         public void OnGet()
         {
-
+            BlaBla = new List<string>()
+            {
+                "Eins",
+                "Zwei",
+                "Drie",
+                "Vier"
+            };
         }
     }
 }
