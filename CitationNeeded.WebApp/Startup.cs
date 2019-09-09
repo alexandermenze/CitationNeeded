c1ddca5a3892294e72f09438ceffa41ce0cc7a91
Alexander Menze <a.menze@polipol.de>
2019-09-09 13:27:33 +0200

    Added database to services

--- CitationNeeded.WebApp/Startup.cs
+++ CitationNeeded.WebApp/Startup.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using CitationNeeded.Database.Database;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CitationNeeded.WebApp
@@ -9,6 +11,9 @@ namespace CitationNeeded.WebApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddDbContext<CitationContext>(
+                o => o.UseMySql(@"Server=localhost;Database=citationneeded;Uid=root;Pwd='c_?h0W0EWInXm\;dPtT~O< ^w7VVqN';"));
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
