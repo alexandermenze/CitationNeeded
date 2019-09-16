a51c0f968fbe5505600dbd8aec51f9bf1043d996
Alexander Menze <a.menze@polipol.de>
2019-09-16 12:51:36 +0200

    Created empty account pages

---
+++ CitationNeeded.WebApp/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CitationNeeded.WebApp.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
