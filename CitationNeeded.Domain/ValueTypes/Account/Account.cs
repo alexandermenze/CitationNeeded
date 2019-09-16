99027e4ed1c3462414d32566292cd59ecf455803
Alexander Menze <a.menze@polipol.de>
2019-09-16 12:58:52 +0200

    Created account class which should store credentials

---
+++ CitationNeeded.Domain/ValueTypes/Account/Account.cs
@@ -0,0 +1,10 @@
+﻿namespace CitationNeeded.Domain.ValueTypes.Account
+{
+    public class Account
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public bool IsEmailVerified { get; set; }
+        public string HashedPassword { get; set; }
+    }
+}
