0b6315c581d26b60cd3fe119596ed39bbbcedc06
Alexander Menze <a.menze@polipol.de>
2019-09-16 13:54:13 +0200

    Created basic login view example

---
+++ CitationNeeded.Domain/ValueTypes/Account/Credentials.cs
@@ -0,0 +1,8 @@
+﻿namespace CitationNeeded.Domain.ValueTypes.Account
+{
+    public class Credentials
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
--- CitationNeeded.WebApp/Pages/Account/Login.cshtml.cs
+++ CitationNeeded.WebApp/Pages/Account/Login.cshtml.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
 
 namespace CitationNeeded.WebApp.Pages.Account
 {
     public class LoginModel : PageModel
     {
-        public void OnGet()
-        {
+        [BindProperty]
+        [Required]
+        public string Username { get; set; }
+        [BindProperty]
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
 
+        public async Task OnPostAsync()
+        {
+            
         }
     }
 }
