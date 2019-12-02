b9aad84dd0e911def566982bc5736109f2e2cf83
Alexander Menze <a.menze@polipol.de>
2019-12-02 14:59:16 +0100

    Added authorize attribute

--- CitationNeeded.WebApp/Pages/Citations.cshtml.cs
+++ CitationNeeded.WebApp/Pages/Citations.cshtml.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CitationNeeded.WebApp.Pages
 {
+    [Authorize]
     public class CitationsModel : PageModel
     {
         public void OnGet()
