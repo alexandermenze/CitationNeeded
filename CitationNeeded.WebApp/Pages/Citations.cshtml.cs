dd767c9accf4b80b0410365d95b306a497ddaa98
Alexander Menze <a.menze@polipol.de>
2019-12-02 14:58:03 +0100

    Created citations page

---
+++ CitationNeeded.WebApp/Pages/Citations.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CitationNeeded.WebApp.Pages
+{
+    public class CitationsModel : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
