51cbde2a2882ab747636542e1c2e933906f7c29f
Alexander Menze <a.menze@polipol.de>
2019-12-06 19:17:02 +0100

    Added citationBooks to index page

--- CitationNeeded.WebApp/Pages/Index.cshtml.cs
+++ CitationNeeded.WebApp/Pages/Index.cshtml.cs
@@ -1,7 +1,11 @@
-﻿using CitationNeeded.Domain.Exceptions;
+﻿using CitationNeeded.Database.Database;
+using CitationNeeded.Domain.Exceptions;
 using CitationNeeded.Domain.Interfaces;
+using CitationNeeded.Domain.ValueTypes;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CitationNeeded.WebApp.Pages
 {
@@ -9,12 +13,15 @@ namespace CitationNeeded.WebApp.Pages
     public class IndexModel : PageModel
     {
         private readonly IIdentityService _identityService;
+        private readonly CitationContext _citationContext;
 
         public Domain.ValueTypes.Account Account { get; set; }
+        public IEnumerable<CitationBook> CitationBooks { get; set; }
 
-        public IndexModel(IIdentityService identityService)
+        public IndexModel(IIdentityService identityService, CitationContext citationContext)
         {
             _identityService = identityService;
+            _citationContext = citationContext;
         }
 
         public void OnGet()
@@ -22,6 +29,7 @@ namespace CitationNeeded.WebApp.Pages
             try
             {
                 Account = _identityService.GetIdentity();
+                CitationBooks = _citationContext.CitationBooks.ToList();
             }
             catch (IdentityException ex)
             {
