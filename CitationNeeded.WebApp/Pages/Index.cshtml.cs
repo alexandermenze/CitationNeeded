bafc53f089d32c4a28268b958f06b3a63b5bfac2
Alexander Menze <a.menze@polipol.de>
2019-12-06 23:11:16 +0100

    Creating new citation books

--- CitationNeeded.WebApp/Pages/Index.cshtml.cs
+++ CitationNeeded.WebApp/Pages/Index.cshtml.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -21,6 +22,10 @@ namespace CitationNeeded.WebApp.Pages
 
         public Domain.ValueTypes.Account Account { get; set; }
         public IEnumerable<CitationBook> CitationBooks { get; set; }
+
+        [BindProperty]
+        [Required]
+        public string CreateBookName { get; set; }
 
         public IndexModel(IIdentityService identityService, CitationContext citationContext)
         {
@@ -37,6 +42,7 @@ namespace CitationNeeded.WebApp.Pages
                     .CitationBooks
                     .Include(b => b.CitationGroups)
                     .ThenInclude(c => c.Citations)
+                    .OrderBy(b => b.Name)
                     .ToListAsync();
             }
             catch (IdentityException ex)
@@ -47,9 +53,23 @@ namespace CitationNeeded.WebApp.Pages
             return Page();
         }
 
+        public async Task<IActionResult> OnPostCreateBookAsync()
+        {
+            _citationContext.CitationBooks.Add(
+                new CitationBook { Name = CreateBookName });
+
+            await _citationContext.SaveChangesAsync();
+
+            return Page();
+        }
+
+        [NonHandler]
         public DateTime GetLatestDate(CitationBook book)
         {
-            return book?.CitationGroups?.Max(c => c.Created) ?? DateTime.Now;
+            if (book.CitationGroups == null || book.CitationGroups.Count() == 0)
+                return DateTime.Now;
+
+            return book.CitationGroups.Max(c => c.Created);
         }
     }
 }
